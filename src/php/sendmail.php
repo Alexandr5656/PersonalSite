ca61edc9d4f503e6c576a19096b292bc627cebc2
Alexandr5656 <alexburbano5656@gmail.com>
2021-11-26 21:25:30 -0500

    New Lines

--- src/php/sendmail.php
+++ src/php/sendmail.php
@@ -56,7 +56,7 @@ $headers = 'Reply-To: '.$from;
 if ($json['status']  === 'success') {  
   if (!@mail($to_myemail, $subject, $message,  $headers)) {
     $m_err = error_get_last()['message'];  
-    $json = my_set_error($json, 'Unable to send Email! \nERRORS:    '.$m_err.'\nEMAIL:    '.$to_myemail.'\n eSUBJECT:     '.$subject.'\nMESSAGE:     '.$message.'\nHEADERS:        '.$headers);
+    $json = my_set_error($json, 'Unable to send Email! '."\r\n".'ERRORS:    '.$m_err."\r\n".'\nEMAIL:    '.$to_myemail."\r\n".'eSUBJECT:     '.$subject."\r\n".'\nMESSAGE:     '.$message."\r\n".'\nHEADERS:        '.$headers);
   }  
 }
 
